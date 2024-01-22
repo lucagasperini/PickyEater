cf3eac02b816aabbcd087eb0c01e4c7148c633e2
CristianaGiustiniani <cristianagiustiniani@gmail.com>
2024-01-22 19:00:58 +0100

    Aggiunte ReportBean e ReportType

---
+++ src/main/java/com/pickyeaters/logic/model/ReportType.java
@@ -0,0 +1,7 @@
+package com.pickyeaters.logic.model;
+
+public enum ReportType {
+    MISSING_INGREDIENT,
+    WRONG_DISH_DESCRIPTION_UNMENTIONED_INGREDIENT,
+    WRONG_DISH_DESCRIPTION_INEXISTING_INGREDIENT;
+}
---
+++ src/main/java/com/pickyeaters/logic/model/bean/ReportBean.java
@@ -0,0 +1,11 @@
+package com.pickyeaters.logic.model.bean;
+
+import com.pickyeaters.logic.model.ReportType;
+import com.pickyeaters.logic.model.User;
+
+public class ReportBean {
+    private String reportId;
+    private ReportType reportType;
+    private User reporter;
+
+}
