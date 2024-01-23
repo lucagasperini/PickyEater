fee2623488f370fef54e2e4c6f3d2bb880a499e1
Luca Gasperini <dev@xsoftware.it>
2024-01-23 23:19:09 +0100

    Add locale PICKY_GUI_LOGIN_LOGIN

--- src/main/java/com/pickyeaters/app/view/gui/LoginView.java
+++ src/main/java/com/pickyeaters/app/view/gui/LoginView.java
@@ -8,12 +8,9 @@ import com.pickyeaters.logic.controller.exception.LoginControllerException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.control.Alert;
 
 public class LoginView extends VirtualView {
     public LoginView(MainController controller) {
@@ -30,7 +27,8 @@ public class LoginView extends VirtualView {
     private TextField inputLoginUsername;
     @FXML
     private PasswordField inputLoginPassword;
-
+    @FXML
+    private Button inputLogin;
     @FXML
     private Label labelLoginUsername;
     @FXML
@@ -39,6 +37,7 @@ public class LoginView extends VirtualView {
     private void setup() {
         labelLoginUsername.setText(SettingsController.i18n("PICKY_GUI_LOGIN_USERNAME"));
         labelLoginPassword.setText(SettingsController.i18n("PICKY_GUI_LOGIN_PASSWORD"));
+        inputLogin.setText(SettingsController.i18n("PICKY_GUI_LOGIN_LOGIN"));
     }
     @FXML protected void clickLogin(ActionEvent event) {
 
