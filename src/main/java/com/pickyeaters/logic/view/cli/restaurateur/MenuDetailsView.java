1e76eefb71f6b91bdedd3f2e3bae777b87d8727a
Luca Gasperini <dev@xsoftware.it>
2024-02-01 20:23:30 +0100

    Add template MenuDetailsView

--- src/main/java/com/pickyeaters/logic/view/cli/restaurateur/MenuDetailsView.java
+++ src/main/java/com/pickyeaters/logic/view/cli/restaurateur/MenuDetailsView.java
@@ -20,11 +20,46 @@ public class MenuDetailsView extends VirtualRequestView {
 
     @Override
     protected boolean request(String request) {
-        return false;
+        switch (request) {
+            case "show", "s":
+                showMenu();
+                return true;
+            case "edit", "e":
+                editDish();
+                return true;
+            case "add", "a":
+                addDish();
+                return true;
+            case "remove", "r":
+                removeDish();
+                return true;
+            default:
+                return false;
+        }
     }
 
     @Override
     protected String requestHelp() {
+        return """
+                [show, s]
+                [edit, e]
+                [add, a]
+                [remove, r]""";
+    }
+
+    private void showMenu() {
+        throw new UnsupportedOperationException();
+    }
+
+    private void editDish() {
+        throw new UnsupportedOperationException();
+    }
+
+    private void addDish() {
+        throw new UnsupportedOperationException();
+    }
+
+    private void removeDish() {
         throw new UnsupportedOperationException();
     }
 }
