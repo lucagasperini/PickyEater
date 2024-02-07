28276bd05c89dbb69899f68797e7e2f916a628ac
CristianaGiustiniani <cristianagiustiniani@gmail.com>
2024-02-07 13:52:26 +0100

    Modificata la funzione setup in modo da mostrare i testi dalle properties; rimossi alcuni import di classi non utilizzate, i quali generavano delle rule violations

--- src/main/java/com/pickyeaters/logic/view/gui/StartView.java
+++ src/main/java/com/pickyeaters/logic/view/gui/StartView.java
@@ -1,12 +1,9 @@
 package com.pickyeaters.logic.view.gui;
 
-import com.pickyeaters.logic.controller.exception.DatabaseControllerException;
-import com.pickyeaters.logic.controller.exception.SettingsControllerException;
+import com.pickyeaters.logic.controller.application.SettingsController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -30,6 +27,9 @@ public class StartView extends VirtualPaneView {
 
     @Override
     protected void setup(Map<String, String> arg) {
+        textWelcome.setText(SettingsController.i18n("START_WELCOME"));
+        buttonRegistration.setText(SettingsController.i18n("START_SIGNUP"));
+        buttonLogin.setText(SettingsController.i18n("START_SIGNIN"));
         if(arg != null) {
             setupLogin(arg.get("login"));
         }
